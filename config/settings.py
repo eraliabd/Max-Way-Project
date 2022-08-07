cf571649413a0da10b0c103c9532dd22f0aed986
Erali Abdinazarov <91982815+eraliabd@users.noreply.github.com>
2022-08-07 20:53:16 +0500

    psql database delete

--- config/settings.py
+++ config/settings.py
@@ -79,17 +79,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': "maxway_db",
-#         'USER': "maxway",
-#         'PASSWORD': 'eralimaxway',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
